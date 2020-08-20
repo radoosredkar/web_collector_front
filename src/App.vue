700c8b71708f1fb29b94cc109d5e4ecf618a0c3a
rado <radoosredkar@hotmail.com>
2020-08-20 18:45:30 +0200

    Add employee working

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+		<div id="app">
+
+				<h1>Employees</h1>
+				<employee-form @add:employee="addEmployee" />
+						<employee-table v-bind:employees="employees" />
+								ttt
+								{{hello}}
+		</div>
+</template>
+
+<script>
+import EmployeeTable from '@/components/EmpoyeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
+
+export default {
+		name: 'App',
+		components: {
+				EmployeeForm,
+				EmployeeTable
+		},
+		data() {
+				return {
+						employees: [
+								{
+										id: 1,
+										name: 'Richard Hendricks',
+										email: 'richard@piedpiper.com',
+								},
+								{
+										id: 2,
+										name: 'Bertram Gilfoyle',
+										email: 'gilfoyle@piedpiper.com',
+								},
+								{
+										id: 3,
+										name: 'Dinesh Chugtai',
+										email: 'dinesh@piedpiper.com',
+								},
+								{
+										id: 4,
+										name: 'Rado Osredkar',
+										email: 'rado.osredkar@gmail.com',
+								},
+						],
+				}
+		},
+		methods:{
+				addEmployee(employee) {
+						const lastId =
+								this.employees.length > 0
+								? this.employees[this.employees.length - 1].id
+								: 0;
+						const id = lastId + 1;
+						const newEmployee = { ...employee, id };
+
+						this.employees = [...this.employees, newEmployee];
+				}
+		},
+
+
+}
+</script>
+
+<style>
+#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+}
+</style>
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="employee-form">
+		<form @submit.prevent="handleSubmit">
+			<label>Employee Name</label>
+			<input type="text" v-model="employee.name" />
+			<label>Employee Email</label>
+			<input type="text" v-model="employee.email" />
+			<button>Add Employee</button>
+		</form>
+  </div>
+</template>
+
+<script>
+export default {
+		name: 'employee-form',
+		data() {
+			return {
+				employee: {
+							employee:'',
+							email:''
+					}
+				}
+		},
+		methods: {
+			handleSubmit() {
+				this.$emit('add:employee', this.employee);
+				}
+		}	
+}
+</script>
+
+<style scoped>
+	margin-bottom: 2rem;
+</style>
+
