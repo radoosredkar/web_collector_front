d9d3eebf26e2d2475928f8e61a3fbd33df51a49e
rado <radoosredkar@hotmail.com>
2020-09-05 11:39:46 +0200

    Fixed image hover

--- src/components/HomesTable.vue
+++ src/components/HomesTable.vue
@@ -1,15 +1,15 @@
 <template>
 		<div id="homes-table">
-				<table>
+				<table class="zui-table">
 						<thead>
 								<tr>
-										<th>Id</th>
+										<th>#</th>
 										<th>Title</th>
 										<th>Description</th>
 										<th>Price</th>
+										<th>Image</th>
 										<th>Source</th>
 										<th>Date Created</th>
-										<th>Image</th>
 								</tr>
 						</thead>
 						<tbody>
@@ -18,13 +18,13 @@
 										<td>{{home.title}}</td>
 										<td>{{home.description}}</td>
 										<td>{{home.price}}</td>
+										<td>
+												<a target="__blank__" :href="home.advUrl">
+														<img :src="home.image" class="image">
+												</a>
+										</td>
 										<td>{{home.source}}</td>
 										<td>{{home.dateCreated}}</td>
-										<td>
-											<a target="__blank__" :href="home.advUrl">
-												<img :src="home.image" style="width:200px;height:200px;">
-											</a>
-										</td>
 								</tr>
 						</tbody>
 				</table>
@@ -44,6 +44,39 @@ export default {
 		}
 }
 </script>
+<style scoped>
+.image {
+		z-index:10;
+		width:200px;
+		height:200px;
+		transition: transform .2s;
 
-<style scoped></style>
+}
+.image:hover {
+
+		-ms-transform: scale(2); /* IE 9 */
+		-webkit-transform: scale(2); /* Safari 3-8 */
+		transform: scale(2); 
+}
+.zui-table {
+		border: solid 1px #DDEEEE;
+		border-collapse: collapse;
+		border-spacing: 0;
+		font: normal 13px Arial, sans-serif;
+}
+.zui-table thead th {
+		background-color: #DDEFEF;
+		border: solid 1px #DDEEEE;
+		color: #336B6B;
+		padding: 10px;
+		text-align: left;
+		text-shadow: 1px 1px 1px #fff;
+}
+.zui-table tbody td {
+		border: solid 1px #DDEEEE;
+		color: #333;
+		padding: 10px;
+		text-shadow: 1px 1px 1px #fff;
+}
+</style>
 
