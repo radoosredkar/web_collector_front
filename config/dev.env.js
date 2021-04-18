eba0883d3383d272ca93f4f7490c661681d7717b
rado <radoosredkar@hotmail.com>
2021-04-18 11:24:45 +0200

    Add client to refresh

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  URL_REFRESH: '"http://localhost:5000/refresh"',
+  URL_REFRESH: '"http://localhost:5000/refresh/frontend"',
   URL_BATCH: '"http://localhost:5000/graphql/batch"',
   URL_UPDATE: '"http://localhost:5000/homes"'
 })
